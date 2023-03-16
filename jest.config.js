614252976675bbd0f4da90b95c1f7ddebce3667b
Lev361 <levimaycon361@gmail.com>
2023-03-15 23:43:03 -0300

    ensure LocalSavePurchases does not delete cache upon creation

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir/src/$1>'
+    }
+}
